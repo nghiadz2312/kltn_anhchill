fc71f7101d1124e1733c54b7662d582d2a381a51
Nghia Nguyen <nghia23122004@gmail.com>
2026-05-08 17:21:48 +0700

    Fix: import usePathname in Profile page

--- kltn_anhchill/app/profile/page.tsx
+++ kltn_anhchill/app/profile/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect, useState } from 'react';
 import { useAuth } from '@/components/AuthContext';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 import Link from 'next/link';
 
 /**
